b66741cc4926044df6a39f1ce808e092d1daad33
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2024-07-16 11:52:14 +0530

    Time: 1 ms (99.60%) | Memory: 44.9 MB (67.72%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i=1;i<=n;i++)
+        {
+            if(i%15==0)
+            {
+                answer.add(i-1,"FizzBuzz");
+            }
+            else if(i%3==0)
+            {
+                answer.add(i-1,"Fizz");
+            }
+            else if(i%5==0)
+            {
+                answer.add(i-1,"Buzz");
+            }
+            else
+            {
+                answer.add(i-1,String.valueOf(i));
+            }
+
+
+        }
+        return answer;
+
+    }
+}
